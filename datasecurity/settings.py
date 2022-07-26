fd07b56adb467774792266e98b62477c052292be
Ken Broni <kbroni123@gmail.com>
2022-07-26 22:05:52 +0000

    'another'

--- datasecurity/settings.py
+++ datasecurity/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,8 +123,9 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR/'staticfiles']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
